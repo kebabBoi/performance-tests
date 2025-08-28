f4d71be8652f5306d497e3eca6b78930290f057c
kebabBoi <gematogen2002@gmail.com>
2025-08-28 10:50:31 +0300

    Подготовка HTTP API клиента для Locust. Практика: Создание билдеров для HTTP API клиентов Locust

--- clients/http/gateway/operations/client.py
+++ clients/http/gateway/operations/client.py
@@ -1,8 +1,13 @@
+from locust.env import Environment
+
 from clients.http.client import HTTPClient, HTTPClientExtensions
 
 from httpx import Response, QueryParams
 
-from clients.http.gateway.client import build_gateway_http_client
+from clients.http.gateway.client import (
+    build_gateway_http_client,
+    build_gateway_locust_http_client
+)
 
 from clients.http.gateway.operations.schema import (
     MakeOperationRequestSchema,
@@ -227,3 +232,16 @@ def build_operations_gateway_http_client() -> OperationsGatewayHTTPClient:
     :return: Готовый к использованию CardsGatewayHTTPClient.
     """
     return OperationsGatewayHTTPClient(client=build_gateway_http_client())
+
+# Билдер для нагрузочного тестирования
+def build_cards_gateway_locust_http_client(environment: Environment) -> OperationsGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр OperationsGatewayHTTPClient адаптированного под Locust.
+
+    Клиент автоматически собирает метрики и передаёт их в Locust через хуки.
+    Используется исключительно в нагрузочных тестах.
+
+    :param environment: объект окружения Locust.
+    :return: экземпляр OperationsGatewayHTTPClient с хуками сбора метрик.
+    """
+    return OperationsGatewayHTTPClient(client=build_gateway_locust_http_client(environment))
