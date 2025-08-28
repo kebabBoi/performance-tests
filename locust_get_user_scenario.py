8e003b91afc8d9f455a7f5c5da419fd420dae553
kebabBoi <gematogen2002@gmail.com>
2025-08-28 11:53:27 +0300

    Использование HTTP API клиентов в нагрузочных сценариях

--- locust_get_user_scenario.py
+++ locust_get_user_scenario.py
@@ -1,31 +1,27 @@
-from locust import HttpUser, between, task
+from locust import User, between, task
 
-from tools.fakers import fake  # генератор случайных данных
+from clients.http.gateway.users.client import build_users_gateway_locust_http_client, UsersGatewayHTTPClient
+from clients.http.gateway.users.schema import CreateUserResponseSchema
 
 
-class GetUserScenarioUser(HttpUser):
+class GetUserScenarioUser(User):
+    # Обязательное поле, требуемое Locust. Будет проигнорировано, но его нужно указать, иначе будет ошибка запуска.
+    host = "localhost"
+
     # Пауза между запросами для каждого виртуального пользователя (в секундах)
     wait_time = between(1, 3)
 
-    # В этой переменной будем хранить данные созданного пользователя
-    user_data: dict
+    users_gateway_client: UsersGatewayHTTPClient
+    create_users_response: CreateUserResponseSchema
 
     def on_start(self) -> None:
         """
         Метод on_start вызывается один раз при запуске каждой сессии виртуального пользователя.
         Здесь мы создаем нового пользователя, отправляя POST-запрос к /api/v1/users.
         """
-        request = {
-            "email": fake.email(),
-            "lastName": fake.last_name(),
-            "firstName": fake.first_name(),
-            "middleName": fake.middle_name(),
-            "phoneNumber": fake.phone_number()
-        }
-        response = self.client.post("/api/v1/users", json=request)
+        self.users_gateway_client = build_users_gateway_locust_http_client(self.environment)
 
-        # Сохраняем полученные данные, включая ID пользователя
-        self.user_data = response.json()
+        self.create_users_response = self.users_gateway_client.create_user()
 
     @task
     def get_user(self):
@@ -33,7 +29,4 @@ class GetUserScenarioUser(HttpUser):
         Основная нагрузочная задача: получение информации о пользователе.
         Здесь мы выполняем GET-запрос к /api/v1/users/{user_id}.
         """
-        self.client.get(
-            f"/api/v1/users/{self.user_data['user']['id']}",
-            name="/api/v1/users/{user_id}"  # Явное указание имени группы запросов
-        )
+        self.users_gateway_client.get_user(self.create_users_response.user.id)
